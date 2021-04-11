81ee6f6357aa7780d60acc1bd65d889d454aa564
Ankush menat <me@ankush.dev>
2021-04-11 07:29:35 +0000

    check status before updating, startdate to  config

--- slack_week_updater/update_week_num.py
+++ slack_week_updater/update_week_num.py
@@ -3,7 +3,6 @@
 This script updates week number in Slack workspace.
 
 This can be deployed using simple cron job or AWS CloudWatch + Lambda.
-Triggered every Sunday 6 PM IST.
 """
 import slack
 import datetime
@@ -11,12 +10,21 @@ import config
 from slack.errors import SlackApiError
 
 
-def update_channel_description(client, channel: str, topic: str):
+
+def update_channel_description(client, channel: str, new_week: int):
     """
         Update slack channel's description.
 
         Notifies author of script if there's issue with running the script.
     """
+
+    current_topic = get_current_topic(client, channel)
+
+    if f"[week {new_week}]" in current_topic.lower():
+        print("Same topic already set, ignoring")
+        return
+
+    topic = f"[Week {new_week}] |  https://world-class.github.io/ | https://github.com/world-class/notes"
 
     try:
         client.conversations_setTopic(channel=channel, topic=topic)
@@ -27,11 +35,20 @@ def update_channel_description(client, channel: str, topic: str):
         )
 
 
+def get_current_topic(client, channel: str) -> str:
+    try:
+        channel_info = client.conversations_info(channel=channel)
+        return channel_info["channel"]["topic"]["value"] or ""
+    except Exception as e:
+        # this is hacky script, don't expect too much!
+        return ""
+
+
 def get_next_week() -> int:
     """
         Get new week number.
     """
-    start_date = datetime.date(2020, 10, 12)
+    start_date = config.start_date
     today = datetime.date.today()
     week = round((today - start_date).days / 7) + 1
     return week
@@ -46,7 +63,7 @@ if __name__ == "__main__":
         update_channel_description(
             client,
             config.slack_general_channel_id,
-            f"[Week {new_week}] |  https://world-class.github.io/ | https://github.com/world-class/notes",
+            new_week
         )
     else:
         print("Invalid week")
